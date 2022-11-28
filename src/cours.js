003a6f2d26212b84548ec1f4fb3e9f277db7950a
HadiqaNazishRaja <111972894+HadiqaNazishRaja@users.noreply.github.com>
2022-11-28 16:26:17 +0000

    Project file added

---
+++ src/cours.js
@@ -0,0 +1,7 @@
+let fullName = prompt("What is your full name?");
+let age = prompt("What is your age?");
+if (age < 15) {
+  alert(`Sorry $(fullName)! You can't join the course`);
+} else {
+  alert(`Thanks ${fullName}! Welcome to the course`);
+}
